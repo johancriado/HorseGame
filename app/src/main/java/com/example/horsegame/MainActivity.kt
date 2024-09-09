27e580b7af00eb34eb3c219d55e2584b253bd96f
johancriado <johancriado@wposs.com>
2024-09-09 17:00:27 -0500

    Redimensionar elementos y quotar ActionBar

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.horsegame
 
+import android.graphics.Point
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+        initScreegame()
+    }
+
+    private fun initScreegame() {
+        setSizeBoard()
+        hideMessage()
+    }
+
+    private fun hideMessage() {
+        var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
+        lyMessage.visibility = View.INVISIBLE
+    }
+
+    private fun setSizeBoard() {
+        var iv: ImageView
+
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width = size.x
+
+        var width_dp = (width / resources.displayMetrics.density)
+
+        val lateralMarginDP = 0
+        val widht_cell = (width_dp - lateralMarginDP)/8
+        val heigth_cell = widht_cell
+
+        for (i in 0..7){
+            for (j in 0..7){
+
+                iv = findViewById(resources.getIdentifier("c$i$j","id",packageName))
+
+                var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, heigth_cell, resources.displayMetrics).toInt()
+                var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, heigth_cell, resources.displayMetrics).toInt()
+                iv.layoutParams = TableRow.LayoutParams(width,height)
+
+            }
+        }
     }
 }
