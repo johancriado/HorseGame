3b583240c9aaf059bd716dfbafc1df98e022c1eb
johancriado <johancriado@wposs.com>
2024-09-10 11:31:38 -0500

    Posicion aleatoria

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -9,10 +9,15 @@ import android.widget.LinearLayout
 import android.widget.TableRow
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var cellSelected_x = 0
+    private var cellSelected_y = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +28,32 @@ class MainActivity : AppCompatActivity() {
             insets
         }*/
         initScreegame()
+        setFirstPosition()
+    }
+
+    private fun setFirstPosition() {
+        var x = 0
+        var y = 0
+        x = (0..7).random()
+        y = (0..7).random()
+
+        cellSelected_x = x
+        cellSelected_y = y
+        selectCell(x, y)
+    }
+
+    private fun selectCell(x: Int, y: Int) {
+        paintHorseCell(cellSelected_x, cellSelected_y, "previus_cell")
+        cellSelected_x = x
+        cellSelected_y = y
+        paintHorseCell(x, y, "selected_cell")
+
+    }
+
+    private fun paintHorseCell(x: Int, y: Int, color: String) {
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
+        iv.setBackgroundColor(ContextCompat.getColor(this, resources.getIdentifier(color, "color",packageName)))
+        iv.setImageResource(R.drawable.caballo)
     }
 
     private fun initScreegame() {
